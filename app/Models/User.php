1f34d8781edbe04aca54cd1a21df7c5e21c285da
Kudinov Kirill <kudinov_kirill@mail.ru>
2018-07-12 18:47:04 +0300

    add jwt auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
-class User extends Model
+class User extends Authenticatable
 {
     protected $table = 'users';
     /**
